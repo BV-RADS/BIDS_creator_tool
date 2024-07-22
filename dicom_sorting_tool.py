17359742c683964cd66b8a473f807cdfad1efcbd
navalpablo <93452498+navalpablo@users.noreply.github.com>
2024-07-22 08:31:27 +0200

    Update dicom_sorting_tool.py
    includes decompress.py

--- dicom_sorting_tool.py
+++ dicom_sorting_tool.py
@@ -43,7 +43,6 @@ def anonymize_dicom_tags(dataset, id_map=None):
     return dataset
 
 def generate_unique_filename(directory, filename):
-    """Generates a unique filename to avoid overwriting in the specified directory."""
     base_name, extension = os.path.splitext(filename)
     counter = 1
     new_filename = filename
@@ -53,14 +52,20 @@ def generate_unique_filename(directory, filename):
     return new_filename
 
 def sanitize_series_description(description):
-    """Sanitizes the series description by replacing spaces with underscores and removing invalid characters."""
     description = description.replace(' ', '_').replace('*', '').replace('.', '_')
-    # Remove or replace other invalid characters
     invalid_chars = r'<>:"/\|?*'
     description = re.sub(f'[{re.escape(invalid_chars)}]', '', description)
     return sanitize_filepath(description, platform='auto')
 
-def copy_dicom_image(src_file, dest_base_dir, pattern, anonymize=False, id_map=None):
+def decompress_dataset(dataset):
+    try:
+        # Attempt to decompress the dataset
+        dataset.decompress()
+    except Exception as e:
+        print(f"Error decompressing dataset: {str(e)}")
+    return dataset
+
+def copy_dicom_image(src_file, dest_base_dir, pattern, anonymize=False, id_map=None, decompress=False):
     non_dicom_extensions = ['.png', '.jpeg', '.jpg', '.gif', '.bmp']
     if any(src_file.lower().endswith(ext) for ext in non_dicom_extensions):
         return
@@ -74,6 +79,9 @@ def copy_dicom_image(src_file, dest_base_dir, pattern, anonymize=False, id_map=N
     if anonymize or id_map:
         dataset = anonymize_dicom_tags(dataset, id_map)
 
+    if decompress:
+        dataset = decompress_dataset(dataset)
+
     for attribute in ['PatientID', 'StudyDate', 'SeriesNumber', 'SeriesDescription']:
         value = get_dicom_attribute(dataset, attribute)
         if attribute == 'SeriesDescription':
@@ -86,38 +94,39 @@ def copy_dicom_image(src_file, dest_base_dir, pattern, anonymize=False, id_map=N
     unique_filename = generate_unique_filename(dest_directory, os.path.basename(src_file))
     dataset.save_as(os.path.join(dest_directory, unique_filename))
 
-def process_file(file, dest_dir, pattern, anonymize, id_map):
-    copy_dicom_image(file, dest_dir, pattern, anonymize, id_map)
+def process_file(file, dest_dir, pattern, anonymize, id_map, decompress):
+    copy_dicom_image(file, dest_dir, pattern, anonymize, id_map, decompress)
 
-def copy_directory(src_dir, dest_dir, pattern, anonymize, id_map):
+def copy_directory(src_dir, dest_dir, pattern, anonymize, id_map, decompress):
     all_files = [os.path.join(root, file) for root, _, files in os.walk(src_dir) for file in files]
     
     num_cores = max(2, multiprocessing.cpu_count() // 2)
     with ProcessPoolExecutor(max_workers=num_cores) as executor:
-        futures = [executor.submit(process_file, file, dest_dir, pattern, anonymize, id_map) for file in all_files]
+        futures = [executor.submit(process_file, file, dest_dir, pattern, anonymize, id_map, decompress) for file in all_files]
         for future in tqdm(as_completed(futures), total=len(futures), desc="Processing", unit="file"):
             future.result()  # To raise exceptions if any
 
-def sort_dicom(input_dir, output_dir, anonymize, id_map):
+def sort_dicom(input_dir, output_dir, anonymize, id_map, decompress):
     pattern = '%PatientID%/%StudyDate%/%SeriesDescription%'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    copy_directory(input_dir, output_dir, pattern, anonymize, id_map)
+    copy_directory(input_dir, output_dir, pattern, anonymize, id_map, decompress)
 
 missing_ids = set()
 
 def main():
-    parser = argparse.ArgumentParser(description="This script copies and optionally anonymizes DICOM files into a structured directory. It can also replace PatientID based on a correlation file.",
+    parser = argparse.ArgumentParser(description="This script copies, optionally anonymizes, and optionally decompresses DICOM files into a structured directory. It can also replace PatientID based on a correlation file.",
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--dicomin', type=str, required=True, help='Path to the input directory containing unsorted DICOM files.')
     parser.add_argument('--dicomout', type=str, required=True, help='Path to the output directory where structured and optionally anonymized DICOM files will be stored.')
     parser.add_argument('--anonymize', action='store_true', help='If specified, anonymizes DICOM tags such as PatientName and PatientBirthDate.')
     parser.add_argument('--ID_correlation', type=str, help='Optional path to a correlation file mapping old PatientIDs to new PatientIDs. \nExpected format: oldID,newID per line.')
+    parser.add_argument('--decompress', action='store_true', help='If specified, decompresses DICOM files during processing.')
     args = parser.parse_args()
 
     id_map = read_id_correlation(args.ID_correlation) if args.ID_correlation else None
 
-    sort_dicom(args.dicomin, args.dicomout, args.anonymize, id_map)
+    sort_dicom(args.dicomin, args.dicomout, args.anonymize, id_map, args.decompress)
 
     if missing_ids:
         log_file_path = 'missing_patient_ids.log'
